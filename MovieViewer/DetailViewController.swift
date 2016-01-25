2e9a99c96e200194a78881932b101fb9d5c7892e
icarlitoss <Carlos_415@Live.com>
2016-01-25 02:04:04 -0800

    Added scrollView + Adjusted the posterView
    scrollView was added so it can fit both the posterView and view(Title +
    Overview)
    
    posterView was adjust to “Aspect Fill”

--- MovieViewer/DetailViewController.swift
+++ MovieViewer/DetailViewController.swift
@@ -17,21 +17,29 @@ class DetailViewController: UIViewController
     @IBOutlet weak var posterView: UIImageView!
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var overviewLabel: UILabel!
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var infoView: UIView!
     //done connecting
     
     var movie: NSDictionary!
     
     
     
-    
     override func viewDidLoad() {
         super.viewDidLoad()
+    
+        //tell the scroll view how big it is
+        // now.. how far is down (need to do an outlte for it..
+        
+        scrollView.contentSize = CGSize(width: scrollView.frame.size.width, height: infoView.frame.origin.y + infoView.frame.size.height)
+        
         
         let title = movie["title"] as? String
         titleLabel.text = title
         
         let overview = movie["overview"]
         overviewLabel.text = overview as? String
+        overviewLabel.sizeToFit()
         
         
         let baseUrl = "http://image.tmdb.org/t/p/w500"
